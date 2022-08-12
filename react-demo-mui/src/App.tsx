97aabbe6afec83166fe1bf2a35c4322e93af6de3
pravinvilaskale <pravinvkale889@gmail.com>
2022-08-12 21:30:02 +0530

    New App

---
+++ react-demo-mui/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Multi from './components/Multi';
+
+function App() {
+  return (
+    <div className="App">
+      <Multi/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-demo-mui/src/components/Multi.tsx
@@ -0,0 +1,12 @@
+import {Typography} from "@mui/material";
+
+const Multi = () =>{
+    return(
+        <div>
+            <Typography variant='h1'>h1 heading</Typography>
+            <Typography variant='h2'>h2 heading</Typography>
+            <Typography variant='h3'>h3 heading</Typography>
+        </div>
+    )
+}
+export default Multi;
